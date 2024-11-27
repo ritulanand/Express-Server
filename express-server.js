ba1b9128e3df8f8056b8206a3db0d56678a583ff
Your Name <you@example.com>
2024-11-27 23:18:02 +0530

    post put get request added

--- express-server.js
+++ express-server.js
@@ -6,8 +6,27 @@ const server = express();
 
 //listen for default request
 
+server.get("/", (req,res, next) => {
+    console.log("first middleare hit");
+    next();
+})
+
 server.get("/", (req,res) => {
-    res.send("welcome to express seerver")
+    res.set('Content-Type', 'text/plain');
+   res.send("heello world ! this is  express server")
+})
+
+server.post("/", (req,res) => {
+    res.status(201).send("post request received");
+})
+
+server.put("/", (req,res) => {
+    res.send("put request received");
+})
+
+
+server.delete('/', (req,res) => {
+    res.send('Delete request received');
 })
 
 //listen on specified port
